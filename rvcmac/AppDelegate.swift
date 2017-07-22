73c37a49eb58157dcd6393e7e7f3f02090fdc650
nt <nmtitov@nmtitov.com>
2017-07-22 15:01:14 +0200

    Create status item stub

--- rvcmac/AppDelegate.swift
+++ rvcmac/AppDelegate.swift
@@ -11,8 +11,22 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var item: NSStatusItem!
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        func setupStatusItem() {
+            self.item = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+            self.item.image = #imageLiteral(resourceName: "rvcmac")
+            self.item.highlightMode = true
+            
+            let menu = NSMenu(title: "Menu")
+            menu.addItem(NSMenuItem(title: "Item 1", action: nil, keyEquivalent: ""))
+            menu.addItem(NSMenuItem(title: "Item 2", action: nil, keyEquivalent: ""))
+            menu.addItem(NSMenuItem(title: "Item 3", action: nil, keyEquivalent: ""))
+            
+            self.item.menu = menu
+        }
+        setupStatusItem()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
