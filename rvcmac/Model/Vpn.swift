d128f6b04e485cfdd011ae70662c14a3e983922f
nt <nmtitov@nmtitov.com>
2017-07-22 23:57:35 +0200

    Revert "Revert "Extend Vpn with json parsing method""
    This reverts commit decbc686af347d4383e1ce1164e856ddc462a9f0.

--- rvcmac/Model/Vpn.swift
+++ rvcmac/Model/Vpn.swift
@@ -6,7 +6,10 @@
 //  Copyright © 2017 Ribose. All rights reserved.
 //
 
-import Cocoa
+import Foundation
+import Argo
+import Curry
+import Runes
 
 enum VpnStatus {
     case notConnected
@@ -18,20 +21,31 @@ enum VpnStatus {
 class Vpn {
     
     let title: String
+    let ovpn: String
     var connect: Bool
     var status: VpnStatus
     
-    init(title: String, connect: Bool, status: VpnStatus) {
+    init(title: String, ovpn: String, connect: Bool) {
         self.title = title
+        self.ovpn = ovpn
         self.connect = connect
-        self.status = status
+        self.status = .notConnected
     }
     
     static func demo() -> [Vpn] {
         return [
-            Vpn(title: "staging.foobar.baz", connect: false, status: .notConnected),
-            Vpn(title: "testing.foobar.baz", connect: true, status: .notConnected),
-            Vpn(title: "production.foobar.baz", connect: true, status: .connected)
+            Vpn(title: "staging.foobar.baz", ovpn: "/Users/test/.setup/vpn/test@foobar.baz-staging.ovpn", connect: false),
+            Vpn(title: "testing.foobar.baz", ovpn: "/Users/test/.setup/vpn/test@foobar.baz-testing.ovpn", connect: true),
+            Vpn(title: "production.foobar.baz", ovpn: "/Users/test/.setup/vpn/test@foobar.baz-product.ovpn", connect: true)
         ]
+    }
+}
+
+extension Vpn: Decodable {
+    static func decode(_ json: JSON) -> Decoded<Vpn> {
+        return curry(Vpn.init)
+            <^> json <| "name"
+            <*> json <| "ovpn"
+            <*> json <| "connect"
     }
 }
