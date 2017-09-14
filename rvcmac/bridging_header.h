b55daf18dceaccfd37ed7c10db175118b2d3e411
nt <nmtitov@nmtitov.com>
2017-09-14 12:37:36 +0200

    Add bridging header

---
+++ rvcmac/bridging_header.h
@@ -0,0 +1,16 @@
+//
+//  bridging_header.h
+//  rvcmac
+//
+//  Created by Nikita Titov on 14/09/2017.
+//  Copyright © 2017 Ribose. All rights reserved.
+//
+
+#ifndef bridging_header_h
+#define bridging_header_h
+
+#include <signal.h>
+#include "common.h"
+#include "rvc_shared.h"
+
+#endif /* bridging_header_h */
