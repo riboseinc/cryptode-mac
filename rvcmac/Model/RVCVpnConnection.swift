f6cda29e168f1bf2acbed5136575f1b11674d0e7
nt <nmtitov@nmtitov.com>
2017-09-17 22:43:23 +0200

    Model RVCVpnConnection

---
+++ rvcmac/Model/RVCVpnConnection.swift
@@ -0,0 +1,69 @@
+//
+//  RVCVpnConnection.swift
+//  rvcmac
+//
+//  Created by Nikita Titov on 17/09/2017.
+//  Copyright © 2017 Ribose. All rights reserved.
+//
+
+import Foundation
+import protocol Decodable.Decodable
+import enum Decodable.DecodingError
+import Decodable
+import CocoaLumberjack
+
+// Vpn connection from `rvc list --json`
+// Response sample:
+//{
+//    "code":0,
+//    "data":[
+//    {
+//    "name":"nmtitov",
+//    "profile":"\/Users\/nt\/.rvc\/nmtitov.ovpn",
+//    "auto-connect":false,
+//    "pre-exec-cmd":"\/usr\/bin\/echo nmtitov"
+//    }
+//    ]
+//}
+
+class RVCVpnConnection: Decodable {
+    
+    let name: String
+    let profile: String
+    let autoConnect: Bool
+    let preExecCmd: String
+    
+    required init(name: String, profile: String, autoConnect: Bool, preExecCmd: String) {
+        self.name = name
+        self.profile = profile
+        self.autoConnect = autoConnect
+        self.preExecCmd = preExecCmd
+    }
+    
+    static func decode(_ json: Any) throws -> Self {
+        return try self.init(
+            name: json => "name",
+            profile: json => "profile",
+            autoConnect: json => "autoConnect",
+            preExecCmd: json => "preExecCmd"
+        )
+    }
+}
+
+class RVCVpnConnectionList: Decodable {
+    
+    let code: String
+    let data: [RVCVpnConnection]
+    
+    required init(code: String, data: [RVCVpnConnection]) {
+        self.code = code
+        self.data = data
+    }
+    
+    static func decode(_ json: Any) throws -> Self {
+        return try self.init(
+            code: json => "code",
+            data: json => "data"
+        )
+    }
+}
