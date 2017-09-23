c0e6828bb7de96871b31b306904e914aa4301adb
nt <nmtitov@nmtitov.com>
2017-09-24 00:17:50 +0200

    Generate files

---
+++ rvcmac/Model/Connection+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Connection+CoreDataClass.swift
+//  rvcmac
+//
+//  Created by Nikita Titov on 24/09/2017.
+//  Copyright © 2017 Ribose. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Connection)
+public class Connection: NSManagedObject {
+
+}
